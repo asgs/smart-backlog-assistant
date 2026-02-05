9e1c7dc68ecb853e12d50fb1be722439d4889ca0
Gowri Sankar Andra Suryanarayana <asgosa.eeegct@gmail.com>
2026-02-05 20:10:00 +0530

    Extract the text contents in the ingestion and summarization test scripts to variables for better readability

--- test-scripts/ingest.sh
+++ test-scripts/ingest.sh
@@ -1,5 +1,7 @@
 BASE_URL=http://localhost:8000
 
+TEXT_CONTENT="We have a transport optimization requirement from many of our logistics clients. the goal is to evaluate all the lanes that the carriers could possibly use and provide the most cost-effective ones in terms of the criteria below. 1. lowest lane pricing, 2. highest carrier delivery success rate, and 3. shortest distance. this story is ready to be assigned to development team for implementation"
+
 # Ingest a new doc
-curl -v -XPOST "$BASE_URL/ingest" -d '{"user_input":"We have a transport optimization requirement from many of our logistics clients. the goal is to evaluate all the lanes that the carriers could possibly use and provide the most cost-effective ones in terms of the criteria below. #1 - lowest lane pricing, #2 - highest carrier delivery success rate, and #3 - shortest distance. this story will be assigned to development team to implement these criteria"}' -H "Content-Type: application/json" | jq
+curl -v -XPOST "$BASE_URL/ingest" -d '{"user_input":"'$TEXT_CONTENT'"}' -H "Content-Type: application/json" | jq
 
--- test-scripts/summarize.sh
+++ test-scripts/summarize.sh
@@ -1,5 +1,7 @@
 BASE_URL=http://localhost:8000
 
+TEXT_CONTENT="This story is to track the development efforts required to implement the transport optimization requirement. Break down the tasks involved with a high-level estimate to build and test each and every criterion to achieve the optimization"
+
 # Summarize a query
-curl -v -XPOST "$BASE_URL/summarize" -d '{"user_input":"This story is to track the development efforts required to implement the transport optimization requirement. Break down the tasks involved with a high-level estimate to build and test each and every criterion to achieve the optimization"}' -H "Content-Type: application/json" | jq
+curl -v -XPOST "$BASE_URL/summarize" -d '{"user_input":"'$TEXT_CONTENT'"}' -H "Content-Type: application/json" | jq
 
