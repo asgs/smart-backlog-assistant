fd72de58bac3db3d9730972549ed6e4fbb900951
Gowri Sankar Andra Suryanarayana <asgosa.eeegct@gmail.com>
2026-02-10 12:41:06 +0530

    Clean-up old files that are not necessary now

--- test-scripts/tune.sh
+++
@@ -1,7 +0,0 @@
-BASE_URL=http://localhost:8000
-
-# Tune the model with a sample one-shot prompt
-payload='{"input":"'$1'","output":"'$2'"}'
-echo $payload
-curl -v -XPOST "$BASE_URL/tune" -d "$payload" -H "Content-Type: application/json" | jq
-
